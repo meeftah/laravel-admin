e44e400b92d181ea225a09b3b2f8b5df3a09a3b5
meeftah <miftah.yuda@gmail.com>
2020-12-19 09:57:58 +0700

    add profil siswa

---
+++ resources/views/dashboard/siswa/tk/profil.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('title', 'Profil Siswa TK')
+
+@section('breadcrumb')
+<div class="br-pageheader pd-y-15 pd-l-20">
+    <nav class="breadcrumb pd-0 mg-0 tx-12">
+        <a class="breadcrumb-item" href="#">Profil Siswa</a>
+    </nav>
+</div>
+@endsection
+
+@section('content-header')
+<div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+    <h4 class="tx-gray-800 mg-b-5">Profil Siswa</h4>
+    <p class="mg-b-0">Siswa TKIT</p>
+</div>
+@endsection
+
+@section('content')
+
+@endsection
