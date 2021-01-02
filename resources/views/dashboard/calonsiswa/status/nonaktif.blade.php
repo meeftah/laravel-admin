147a8130ae267a9bd45535a9429e5792c31eb25e
meeftah <miftah.yuda@gmail.com>
2021-01-03 03:03:33 +0700

    add response in dashboard based on status_siswa

---
+++ resources/views/dashboard/calonsiswa/status/nonaktif.blade.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <div class="card-body">
+        <div class="text-justify">
+            <div>
+                <h4 class="text-danger">PERHATIAN!</h4>
+                <p>
+                    Mohon maaf, akun anda di <strong class="text-danger">non-aktifkan</strong> oleh admin 
+                    {{ config('app.nama_sekolah') }}. Silakan hubungi admin agar dapat diaktifkan kembali.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/dashboard/calonsiswa/status/terverifikasi.blade.php
@@ -0,0 +1,9 @@
+<div class="card mb-0">
+    <div class="card-body">
+        <div class="text-justify">
+            Akun anda telah <strong class="text-success">Terverifikasi</strong>. Silakan klik menu 
+            <a href="{{ route('dashboard.calonsiswa.profil', auth()->user()->id) }}">Profil Calon Siswa</a> 
+            melengkapi profil Anda agar dapat melanjutkan ke tahap berikutnya.
+        </div>
+    </div>
+</div>
