38dcf434775cd9cb43d5c5d8e8523ed735b53b58
meeftah <miftah.yuda@gmail.com>
2021-01-03 03:00:56 +0700

    add status_ppdb

---
+++ config/status_ppdb.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'calon_siswa'   => [
+        'terdaftar'     => 'Terdaftar',
+        'nonaktif'      => 'Non Aktif',
+        'datalengkap'   => 'Selesai Melengkapi Data',
+        'terverifikasi' => 'Terverifikasi',
+        'lulus'         => 'Lulus',
+    ],
+    'pendaftaran'   => [
+        'baru'      => 'Siswa Baru',
+        'pindahan'  => 'Siswa Pindahan',
+    ]
+];
