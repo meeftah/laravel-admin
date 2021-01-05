63cf07fdc90d956fa7f8441e84b6991016c62c50
meeftah <miftah.yuda@gmail.com>
2021-01-05 22:23:57 +0700

    rename file

--- resources/views/dashboard/mail/verifikasiva.blade.php
+++
@@ -1,3 +0,0 @@
-<strong>Assalamu'alaikum Warrahmatullahi Wabarakatuh</strong> {{ $username }},
-<br><br>
-Akun anda telah diverifikasi oleh admin PPDB Online Al-Fityan Kubu Raya, silakan melanjutkan pengisian data calon siswa dengan login di <a href="http://ppdb.alfityankuburaya.sch.id/login">Halaman login</a>.
