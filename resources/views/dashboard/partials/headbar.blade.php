0481029b7272a88066cd12f28894a1acc4a6e2e2
meeftah <miftah.yuda@gmail.com>
2021-01-28 15:04:17 +0700

    remove search

--- resources/views/dashboard/partials/headbar.blade.php
+++ resources/views/dashboard/partials/headbar.blade.php
@@ -14,12 +14,6 @@
         <div class="mt-3 ml-3">
             <h5>{{ get_greeting() }}, {{ Auth::user()->username }}</h5>
         </div>
-        {{-- <div class="input-group hidden-xs-down wd-170 transition">
-            <input id="searchbox" type="text" class="form-control" placeholder="Search">
-            <span class="input-group-btn">
-                <button class="btn btn-secondary" type="button"><i class="fa fa-search"></i></button>
-            </span>
-        </div><!-- input-group --> --}}
-    </div><!-- br-header-left -->
-</div><!-- br-header -->
+    </div>
+</div>
 <!-- ########## END: HEAD PANEL ########## -->
