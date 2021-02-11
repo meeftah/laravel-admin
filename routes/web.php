1dcfdd4618e3e37d50f84dce055124afdba7ae96
meeftah <miftah.yuda@gmail.com>
2021-02-11 15:27:36 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,11 @@ Route::group(['namespace' => 'Dashboard', 'middleware' => ['auth'], 'prefix' => 
     Route::get('info-tambahan/api', 'InfoTambahanController@datatableAPI')->name('info-tambahan.api');
     Route::resource('info-tambahan', 'InfoTambahanController');
     Route::post('info-tambahan/delete/gambar/', 'InfoTambahanController@deleteGambar')->name('info-tambahan.delete.gambar');
-
-    Route::get('info-tambahan-daftar/api', 'InfoTambahanDaftarController@datatableAPI')->name('info-tambahan-daftar.api');
-    Route::get('info-tambahan-daftar/{id}', 'InfoTambahanDaftarController@index')->name('info-tambahan-daftar.index');
-    Route::get('info-tambahan-daftar/create/{id}', 'InfoTambahanDaftarController@create')->name('info-tambahan-daftar.create');
-    Route::post('info-tambahan-daftar/create/{id}', 'InfoTambahanDaftarController@store')->name('info-tambahan-daftar.store');
+    
+    // Route Daftar Info Tambahan
+    Route::get('info-tambahan/daftar/api', 'InfoTambahanDaftarController@datatableAPI')->name('info-tambahan-daftar.api');
+    Route::get('info-tambahan/daftar/{id}', 'InfoTambahanDaftarController@index')->name('info-tambahan-daftar.index');
+    Route::get('info-tambahan/daftar/create/{id}', 'InfoTambahanDaftarController@create')->name('info-tambahan-daftar.create');
+    Route::post('info-tambahan/daftar/create/{id}', 'InfoTambahanDaftarController@store')->name('info-tambahan-daftar.store');
+    Route::delete('info-tambahan/daftar/{id}', 'InfoTambahanDaftarController@destroy')->name('info-tambahan-daftar.destroy');
 });
